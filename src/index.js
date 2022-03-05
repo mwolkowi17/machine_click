eb8b57502bbec91eb3d84845a10c0cfbd6e85aad
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2022-03-05 21:58:56 +0100

    next elem

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoade
 import * as THREE from 'three';
 import {OrbitControls} from '../node_modules/three/examples/jsm/controls/OrbitControls';
 import { light2, light3, light4 } from './direct_light.js';
-//import { mixer, action, gltfLoader2 } from './loader';
+import { mixer, action, gltfLoader2 } from './loader';
 
 export const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -13,3 +13,23 @@ renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 scene.background = new THREE.Color( 0xFFFFFF );
+
+camera.position.z = 6;
+camera.position.y = 2;
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+controls.update();
+controls.autoRotate=false;
+const clock= new THREE.Clock();
+
+const animate = function () {
+    requestAnimationFrame(animate);
+    var delta = clock.getDelta(); // clock is an instance of THREE.Clock
+
+    renderer.render(scene, camera);
+    
+  
+    controls.update()
+}
+animate();
