b1d4339f2179a6ccc728568046ed50a564a1b3d2
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2022-03-06 16:39:35 +0100

    action added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoade
 import * as THREE from 'three';
 import {OrbitControls} from '../node_modules/three/examples/jsm/controls/OrbitControls';
 import { light2, light3, light4 } from './direct_light.js';
-import { mixer, action, gltfLoader2 } from './loader';
+import { mixer, action, action2, action3,action4,action5,action6,action7,action8,action9,action10,action11,action12,action13,action14,action15,action16,action17, gltfLoader2 } from './loader';
 
 export const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -61,7 +61,27 @@ function onClick(event) {
     if (intersects.length > 0) {
   
       console.log('Intersection:', intersects[0]);
-      
+      mixer.setTime(0);   
+      mixer.timeScale=1
+      //closed=true;
+     action.play();
+     action2.play();
+     action3.play();
+     action4.play();
+     action5.play();
+     action6.play();
+     action7.play();
+     action8.play();
+     action9.play();
+     action10.play();
+     action11.play();
+     action12.play();
+     action13.play();
+     action14.play();
+     action15.play();
+     action16.play();
+     action17.play();
+
       //const dispalyDetails = document.getElementsByClassName('display')[0];
       //dispalyDetails.style.visibility='visible'
   
